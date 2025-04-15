45186741263f2242142160e0a280982fcf9d7a35
rchrdth1 <68255037+rchrdth1@users.noreply.github.com>
2025-04-15 14:11:35 -0400

    Update trajectory_follower.py

--- path_planning/trajectory_follower.py
+++ path_planning/trajectory_follower.py
@@ -2,6 +2,8 @@ import rclpy
 from ackermann_msgs.msg import AckermannDriveStamped
 from geometry_msgs.msg import PoseArray
 from rclpy.node import Node
+import numpy as np
+import tf_transformations
 
 from .utils import LineTrajectory
 
@@ -33,7 +35,39 @@ class PurePursuit(Node):
                                                1)
 
     def pose_callback(self, odometry_msg):
-        raise NotImplementedError
+
+        # deconstructing pose
+        robot_position = odometry_msg.pose.pose.position
+        robot_orientation = odometry_msg.pose.pose.orientation
+
+        # x, y, yaw
+        robot_x = robot_position.x
+        robot_y = robot_position.y
+        robot_yaw = tf_transformations.euler_from_quaternion([robot_orientation.x, robot_orientation.y, 
+                                                              robot_orientation.z, robot_orientation.w])
+        # check if each point is at lookahead distance
+        for point in self.trajectory.points:
+            dx = point[0] - robot_x
+            dy = point[1] - robot_y
+
+            robot_distance_to_point = np.hypot(dx, dy)
+
+            if robot_distance_to_point >= self.lookahead:
+                robot_lookahead_point = point
+
+        # transforming from global frame to robot frame
+        rframe_lookahead_x = np.cos(-robot_yaw) * (robot_lookahead_point[0] - robot_x) - np.sin(-robot_yaw) * (robot_lookahead_point[1] - robot_y)
+        rframe_lookahead_y = np.sin(-robot_yaw) * (robot_lookahead_point[0] - robot_x) + np.cos(-robot_yaw) * (robot_lookahead_point[1] - robot_y)
+
+        # pure pursuit algorithm
+        curvature = (2 * rframe_lookahead_y) / (self.lookahead ** 2)
+        steering_angle = np.arctan(self.wheelbase_length * curvature)
+
+        # adjusting drive msg and publishing
+        drive_msg = AckermannDriveStamped()
+        drive_msg.drive.speed = self.speed
+        drive_msg.drive.steering_angle = steering_angle
+        self.drive_pub.publish(drive_msg)
 
     def trajectory_callback(self, msg):
         self.get_logger().info(f"Receiving new trajectory {len(msg.poses)} points")
